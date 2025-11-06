0c0d776b7fb97c01ca5eb15a9b0997fcfe18677c
Stéphane GRONDIN <154341995+Darinocan@users.noreply.github.com>
2025-11-06 20:20:59 +0100

    Version complète de game et board Modification de test.c pour pouvoir testerboard et game

--- test.c
+++ test.c
@@ -11,6 +11,33 @@ int main()
     struct game* g = create_game(&player1, &player2, PUBLIC);
     struct board* b = create_board(g);
     ui_draw_board(convert_board_to_string(b), STATE_LOBBY);
+    int turn = 0;
+    while(1) {
+        char input[100];
+        ui_get_input(input, sizeof(input));
+        if (input[0] == 'q') {
+            break;
+        } else if (input[0] == 'p') {
+            printf("Current board state:\n%s\n", convert_board_to_string(b));
+        } else{
+            int res;
+            if (turn == 0) {
+                ui_add_message("Patrick's turn", STATE_INGAME);
+                res = try_a_move(g, input[0], b, &player1);
+                turn = 1;
+            } else {
+                ui_add_message("Spongebob's turn", STATE_INGAME);
+                res = try_a_move(g, input[0], b, &player2);
+                turn = 0;
+            }
+            ui_draw_board(convert_board_to_string(b), STATE_INGAME);
+            ui_add_message("Return code:", STATE_INGAME);
+            char err_msg[50];
+            snprintf(err_msg, sizeof(err_msg), "%d", res);
+            ui_add_message(err_msg, STATE_INGAME);
+        }
+    }
+
     //printf("Hello, World!\n");
     return 0;
 }
